f56278d4d825487ed6bb5011c42761e49ce212cd
fique19 <mugiranezamagni@gmail.com>
2023-03-16 02:13:40 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,21 +6,24 @@
  * @src: input value
  *
  * Return: void
- */
+*/
 char *_strcat(char *dest, char *src)
 {
-	int l, n;
+	int i, j;
 
-	for (n = 0; dest[n] != '\0'; n++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		continue;
+		i++;
 	}
-	for (l = 0; src[l] != '\0'; l++)
+	j = 0;
+	while (src[j] != '\0')
 	{
-		dest[n] = src[l];
-		n++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[n] = '\0';
 
+	dest[i] = '\0';
 	return (dest);
 }
