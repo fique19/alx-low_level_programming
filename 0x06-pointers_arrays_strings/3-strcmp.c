714c43edab517c853ae5a8bf102eba21f3acfb9a
fique19 <mugiranezamagni@gmail.com>
2023-03-17 03:25:31 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,8 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, x;
+	int x;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		continue;
-	}
-	if (i != j)
-		if (i < j)
-			return (-15);
-		else if (i > j)
-			return (15);
 	for (x = 0; x < i; x++)
 	{
 		if (s1[x] != s2[x])
