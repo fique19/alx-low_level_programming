4d4ae164bd9d9a86ac301651da071dd978ca5b00
fique19 <mugiranezamagni@gmail.com>
2023-03-18 05:52:10 -0700

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,20 @@
 */
 char *leet(char *c)
 {
-	int i;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
-		if (c[i] == 'a' || c[i] == 'A')
-			c[i] = 52;
-		else if (c[i] == 'e' || c[i] == 'E')
-			c[i] = 51;
-		else if (c[i] == 'o' || c[i] == 'O')
-			c[i] = 48;
-		else if (c[i] == 't' || c[i] == 'T')
-			c[i] = 55;
-		else if (c[i] == 'l' || c[i] == 'L')
-			c[i] = 49;
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == s1[j])
+			{
+				c[i] = s2[j];
+			}
+		}
+
 	}
 	return (c);
 }
