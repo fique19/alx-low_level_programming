d2b567c5cf87f6a28768b61612fc60be84099036
fique19 <mugiranezamagni@gmail.com>
2023-03-28 13:03:25 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: input value
+ *
+ * Return: Always (0).
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+
+	else
+		_putchar('\n');
+
+}
